21d82383dbfd5953fcd2767e290c2750cdd57940
HiJared <thukyhsu@qq.com>
2019-09-23 12:03:23 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -18,9 +19,24 @@ class UsersController extends Controller
     	return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request,ImageUploadHandler $upload, User $user)
     {
-    	$user->update($request->all());
+    	//dd($request->avatar);
+    	// dd($request->file('avatar'));
+    	$data = $request->all();
+    	
+    	if ($request->avatar) {
+
+    		$result = $upload->save($request->avatar, 'avatars', $user->id);
+
+            if ($result) {
+                
+                $data['avatar'] = $result['path'];
+            }
+    		
+    	}
+
+        $user->update($data);
 
     	return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
