949621249d6e38fb04941c3eab9136913aaae080
HiJared <thukyhsu@qq.com>
2019-09-24 01:37:45 +0000

    分类模型和数据

---
+++ app/Models/Categroy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categroy extends Model
+{
+    //
+    public $timestamps = false;
+    //
+    protected $fillalble = ['name', 'description'];
+}
