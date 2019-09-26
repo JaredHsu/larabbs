97dafde861623b12dfdeb0d2c41f32a6925d3df5
HiJared <thukyhsu@qq.com>
2019-09-26 12:05:26 +0000

    集成laravel-permission扩展包”

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,42 +9,15 @@ use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use App\Models\Topic;
 
 use Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Notifiable, MustVerifyEmailTrait;
 
-    // use Notifiable {
-    //     notify as protected laravelNotify;
-    // }
+    use HasRoles;
 
-    //重写nofify以适应通知
-    //  public function notify($instance)
-    // {
-    //     // 如果要通知的人是当前用户，就不必通知了！
-    //     if ($this->id == Auth::id()) {
-    //         return;
-    //     }
-
-    //     // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
-    //     if (method_exists($instance, 'toDatabase')) {
-    //         $this->increment('notification_count');
-    //     }
-
-    //     $this->laravelNotify($instance);
-    // }
-
-    //替代上面消息通知文案，更优解
-    public function topicNotify($instance)
-        {
-            // 如果要通知的人是当前用户，就不必通知了！
-            if ($this->id == Auth::id()) {
-                return;
-            }
-            $this->increment('notification_count');
-            $this->notify($instance);
-        }
-
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -88,4 +61,36 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+
+    // use Notifiable {
+    //     notify as protected laravelNotify;
+    // }
+
+    //重写nofify以适应通知
+    //  public function notify($instance)
+    // {
+    //     // 如果要通知的人是当前用户，就不必通知了！
+    //     if ($this->id == Auth::id()) {
+    //         return;
+    //     }
+
+    //     // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
+    //     if (method_exists($instance, 'toDatabase')) {
+    //         $this->increment('notification_count');
+    //     }
+
+    //     $this->laravelNotify($instance);
+    // }
+
+    //替代上面消息通知文案，更优解
+    public function topicNotify($instance)
+        {
+            // 如果要通知的人是当前用户，就不必通知了！
+            if ($this->id == Auth::id()) {
+                return;
+            }
+            $this->increment('notification_count');
+            $this->notify($instance);
+        }
+
 }
