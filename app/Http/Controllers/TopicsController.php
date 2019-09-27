c9e587e42a5af5455e3bfe65498e298a480a12be
HiJared <thukyhsu@qq.com>
2019-09-27 08:58:51 +0000

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topic;
+use App\Models\User;
 use Auth;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -17,10 +18,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
-		$topics = $topic->withOrder($request->order)->paginate(20);;
-		return view('topics.index', compact('topics'));
+		$topics = $topic->withOrder($request->order)->paginate(20);
+		$active_users = $user->getActiveUsers();
+
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
